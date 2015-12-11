bc46419b36dabf85cf256dd639ca6afb1c1a2ef5
Abner Zhong <windlessg@gmail.com>
2015-12-11 15:04:09 +0800

    assemble command

--- lib/ijiami.rb
+++ lib/ijiami.rb
@@ -8,23 +8,45 @@ module Ijiami
   class CLI < Thor
     desc 'assemble USERNAME APK_PATH', '上传－加密－下载'
     def assemble(username, apk_path)
+      result = upload(username, apk_path)
+      unless result["code"] == 1100
+        return
+      end
+
+      result = encrypt(username, result["body"]["appId"])
+      unless result["code"] == 1100
+        return
+      end
+
+      result = check(username, result["body"]["appId"])
+      unless result["code"] == 1100
+        return
+      end
+
+      puts "Download url: #{result["downUrl"]}"
     end
 
     desc 'verify USERNAME', '获取用户信息'
     def verify(username)
-      $stdout.puts Ijiami::Api.new(username).verify
+      result = Ijiami::Api.new(username).verify
+      $stdout.puts result
+      result
     end
 
     desc 'upload USERNAME FILE_PATH', '上传 APK'
     def upload(username, file_path)
       full_path = File.expand_path(file_path)
       $stdout.puts "Uploading apk #{full_path}"
-      $stdout.puts Ijiami::Api.new(username).upload_local(File.expand_path(full_path))
+      result = Ijiami::Api.new(username).upload_local(File.expand_path(full_path))
+      $stdout.puts result
+      result
     end
 
     desc 'encrypt USERNAME APP_ID', '加密'
     def encrypt(username, app_id)
-      $stdout.puts Ijiami::Api.new(username).encrypt(app_id)
+      result = Ijiami::Api.new(username).encrypt(app_id)
+      $stdout.puts result
+      result
     end
 
     desc 'check USERNAME APP_ID', '查看加密结果'
@@ -32,10 +54,12 @@ module Ijiami
       result = Ijiami::Api.new(username).encrypt_result(app_id)
       $stdout.puts result
       while result["code"] != 1100
-        sleep 2
+        sleep 5
         result = Ijiami::Api.new(username).encrypt_result(app_id)
         $stdout.puts result
       end
+
+      result
     end
 
   end
