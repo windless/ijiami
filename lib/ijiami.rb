0ebb51bc006d5170d3b14e38544a1e50b7a8d518
Abner Zhong <windlessg@gmail.com>
2015-12-11 14:52:33 +0800

    check 条件才成使用 code

--- lib/ijiami.rb
+++ lib/ijiami.rb
@@ -31,7 +31,7 @@ module Ijiami
     def check(username, app_id)
       result = Ijiami::Api.new(username).encrypt_result(app_id)
       $stdout.puts result
-      while result["downUrl"].to_s == ''
+      while result["code"] != 1100
         sleep 2
         result = Ijiami::Api.new(username).encrypt_result(app_id)
         $stdout.puts result
